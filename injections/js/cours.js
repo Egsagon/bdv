f9c30bf7411b7ee46a7a3ec3adfd86a7ea91bbb9
EGsagon <egsagon12@gmail.com>
2023-11-22 16:29:31 +0100

    added replays and spec presence

--- injections/js/cours.js
+++ injections/js/cours.js
@@ -1,5 +1,13 @@
 /* Injections for /students/cours */
 
+/*
+    Retract pointers level
+    0 - Deploy all
+    1 - Deploy track and termes
+    2 - Deploy track
+*/
+const retract_level = 1
+
 $(window).on('load', () => {
 
     $('#main .pointer').each((i, el) => {
@@ -10,6 +18,13 @@ $(window).on('load', () => {
         $(el).on('click', {p: parent}, (ev) => {
             $(ev.data.p).toggleClass('flipped')
         })
+    })
+
+    ;/*~*/;['ue', 'terme', 'track'].slice(retract_level).forEach(cls => {
+        $(`#main .pointer.collapse_${cls}`).each((i, el) => {
+            console.log('Showing', el.classList)
+            $(el).parent().find('> ol').removeClass('flipped')
+        })  
     })
 })
 
