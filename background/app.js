df6ef7601d34a00c8e60b48a000f34b04b6c06e4
EGsagon <egsagon12@gmail.com>
2023-11-29 14:31:35 +0100

    auto theme on install

---
+++ background/app.js
@@ -0,0 +1,12 @@
+// Define setting on app install
+
+browser.runtime.onInstalled.addListener(async () => {
+
+    console.log('Saving default settings to storage')
+
+    await browser.storage.sync.set({
+        color_scheme: ':root {--bdv-fg-color: #f8f8f2 !important;--bdv-bg-color: #18181a !important;--bdv-bg-color-2: #232327 !important;--bdv-accent-color: #cf0f51 !important;--bdv-border-radius: 10px !important;--bdv-gap: 10px !important;--bdv-ext-gap: 10px !important;}',
+        accent_override: false,
+        disable_logo: false
+    })
+})
