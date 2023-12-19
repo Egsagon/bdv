aa7fb2c7060d56de40c464d39dc624d92825a3dd
EGsagon <egsagon12@gmail.com>
2023-12-19 13:19:33 +0100

    EDT improvements + promotions hotfix

--- injections/js/adfs.js
+++ injections/js/adfs.js
@@ -1,16 +1,3 @@
 /* Injection for the ADFS */
 
 window.bdv.init(false, () => {})
-
-$(window).on('DOMContentLoaded', () => {
-
-    // window.bdv.log('Starting injection')
-
-    $('#loginMessage').html('<h1>Login</h1>')
-
-    $('#introduction').html(`
-        <a onclick="RedirectToPSChange()" href="#">Change password?</a>
-        <br>
-        <a href="https://www.leonard-de-vinci.net/lost_password.php">Forgot password?</a>
-    `)
-})
