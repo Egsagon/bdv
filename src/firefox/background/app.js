1e431ed2f29337b8e624b1713bd030e2fddfb94b
EGsagon <egsagon12@gmail.com>
2024-02-06 14:20:12 +0100

    Fix default theme not applying

--- src/firefox/background/app.js
+++ src/firefox/background/app.js
@@ -1,62 +1,25 @@
 // Define default theme on addon installation
 
-const DEFAULT_THEMES = {
-    alv: {
-        color_scheme: `:root {
-            --bdv-bg-color: #18181a !important;
-            --bdv-bg-color-2: #232327 !important;
-            --bdv-fg-color: #F8F8F2 !important;
-            --bdv-error-color: #FF5555 !important;
-            --bdv-border-radius: 10px !important;
-            --bdv-accent-color: var(--school-accent) !important;
-            --bdv-gap: 10px !important;
-            --bdv-ext-gap: 10px !important;
-            --bdv-logo-border: 10px !important;
-        }`,
-        accent_override: false,
-        disable_logo: true,
-        logo: '$school'
-    },
-
-    bdv: {
-        color_scheme: `:root {
-            --bdv-bg-color: #18181a !important;
-            --bdv-bg-color-2: #232327 !important;
-            --bdv-fg-color: #F8F8F2 !important;
-            --bdv-error-color: #FF5555 !important;
-            --bdv-accent-color: var(--school-accent) !important;
-            --bdv-border-radius: 10px !important;
-            --bdv-gap: 10px !important;
-            --bdv-ext-gap: 10px !important;
-            --bdv-logo-border: 10px !important;
-        }`,
-        accent_override: false,
-        disable_logo: true,
-        logo: 'generic.png'
-    },
-
-    new: {
-        color_scheme: `:root {
-            --bdv-bg-color: #18181a !important;
-            --bdv-bg-color-2: #232327 !important;
-            --bdv-fg-color: #F8F8F2 !important;
-            --bdv-accent-color: #ee5256 !important;
-            --bdv-error-color: #FF5555 !important;
-            --bdv-border-radius: 10px !important;
-            --bdv-gap: 10px !important;
-            --bdv-ext-gap: 10px !important;
-            --bdv-logo-border: 0px !important;
-        }`,
-        accent_override: true,
-        disable_logo: true,
-        logo: 'bdv.png'
-    }
-}
-
 // Default configuration
 const REFRESH_ON_INSTALL = true
 const OPEN_SETTINGS_ON_INSTALL = false
-const DEFAULT_THEME = DEFAULT_THEME.new
+
+const DEFAULT_THEME = {
+    color_scheme: `:root {
+        --bdv-bg-color: #18181a !important;
+        --bdv-bg-color-2: #232327 !important;
+        --bdv-fg-color: #F8F8F2 !important;
+        --bdv-accent-color: #ee5256 !important;
+        --bdv-error-color: #FF5555 !important;
+        --bdv-border-radius: 10px !important;
+        --bdv-gap: 10px !important;
+        --bdv-ext-gap: 10px !important;
+        --bdv-logo-border: 0px !important;
+    }`,
+    accent_override: true,
+    disable_logo: true,
+    logo: 'bdv.png'
+}
 
 browser.runtime.onInstalled.addListener(async () => {
 
