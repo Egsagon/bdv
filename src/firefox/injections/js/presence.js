71b9a18982f895b16f09a6c1bab8affe1e180b38
@Egsagon <egsagon12@gmail.com>
2024-03-13 14:28:37 +0100

    Update on auto presecen

--- src/firefox/injections/js/presence.js
+++ src/firefox/injections/js/presence.js
@@ -1,24 +1,46 @@
 /* Injection for the presence /student/presences/<id> */
 
+const upload_presence = async (page_id) => {
+    // Attempt to upload presence
+
+    data = await $.ajax({
+        url: 'upload.php',
+        type: 'POST',
+        data: {
+            'act': 'set_present',
+            'seance_pk': page_id
+        },
+        cache: false
+    })
+    
+    console.log('Received data:', data)
+    // eval(data)
+}
 
 window.addEventListener('load', () => {
 
-    setter = $('#set-presence')
+    page_id = window.location.href.split('/').slice(-1)[0]
 
     // Don't inject anything if presence is already set
-    if (setter.css('display') !== 'none') return
-    
+    if ($('#body_presence > div.alert-success').length) return
+
     activate = document.createElement('button')
     activate.innerHTML = 'Automatic presence'
     activate.id = 'bdv-auto-presence'
 
-    activate.addEventListener('click', ev => {
-        inerval = setInterval(() => {
-            activate.innerHTML = 'Refreshing...'
+    activate.addEventListener('click', () => {
 
-            // TODO - Fetch page or reload current page
+        // Toggle visibility
+        activate.innerHTML = 'Automatic presence - ENABLED'
+        activate.classList.toggle('active')
 
-        }, 5000)
+        if (!activate.classList.contains('active'))
+            window.location.reload()
+
+        setInterval(async () => {
+            console.log('Attemping to upload presence')
+            await upload_presence(page_id)
+        }, 8000)
     })
 
     $('#body_presence').append(activate)
